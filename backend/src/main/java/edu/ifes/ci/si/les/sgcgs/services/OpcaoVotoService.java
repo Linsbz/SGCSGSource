638539f9a213c7e539f26b430cafc7516a81fce6
Hilda <69116618+HildaBiazatti@users.noreply.github.com>
2022-06-07 00:13:10 -0300

    atualizando services enquete

--- backend/src/main/java/edu/ifes/ci/si/les/sgcgs/services/OpcaoVotoService.java
+++ backend/src/main/java/edu/ifes/ci/si/les/sgcgs/services/OpcaoVotoService.java
@@ -1,10 +1,16 @@
 package edu.ifes.ci.si.les.sgcgs.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import edu.ifes.ci.si.les.sgcgs.model.OpcaoVoto;
 import edu.ifes.ci.si.les.sgcgs.repositories.OpcaoVotoRepository;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import edu.ifes.ci.si.les.sgcgs.services.exceptions.DataIntegrityException;
+import edu.ifes.ci.si.les.sgcgs.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class OpcaoVotoService {
@@ -12,7 +18,13 @@ public class OpcaoVotoService {
     private OpcaoVotoRepository repository;
 
     public OpcaoVoto findById(Integer id) {
-        return repository.findById(id).get();
+        // return repository.findById(id).get();
+        try{
+            OpcaoVoto obj = repository.findById(id).get();
+            return obj;
+        }catch (NoSuchElementException e) {
+            throw new ObjectNotFoundException("OpcaoVoto não encontrada! ID: " + id + ", Tipo: " + OpcaoVoto.class.getName());
+        }
     }
 
     public List<OpcaoVoto> findAll() {
@@ -20,16 +32,32 @@ public class OpcaoVotoService {
     }
 
     public OpcaoVoto insert(OpcaoVoto obj) {
-        return repository.save(obj);
+        // return repository.save(obj);
+        obj.setId(null);
+        try{
+            return repository.save(obj);
+        }catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Campo obrigatório não foi preenchido!");
+        }
     }
 
     public OpcaoVoto update(OpcaoVoto obj) {
         findById(obj.getId());
-        return repository.save(obj);
+        //return repository.save(obj);
+        try{
+            return repository.save(obj);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Campo obrigatório não foi preenchido!");
+        }
     }
 
     public void delete(Integer id) {
         findById(id);
-        repository.deleteById(id);
+        // repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir uma OpcaoVoto que esteja em alguma Enquete aberta!");
+        }
     }
 }
