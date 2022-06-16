1468986eeebfc207385034a77ee2dce00818dd59
Júlia Souza <juliadsouzaborges@gmail.com>
2022-06-16 15:07:16 -0300

    notação do autor da task

--- backend/src/main/java/edu/ifes/ci/si/les/sgcgs/repositories/PostagemNoticiaRepository.java
+++ backend/src/main/java/edu/ifes/ci/si/les/sgcgs/repositories/PostagemNoticiaRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import edu.ifes.ci.si.les.sgcgs.model.PostagemNoticia;
 
+/** @author Júlia de Souza Borges */
+
 @Repository
 public interface PostagemNoticiaRepository extends JpaRepository<PostagemNoticia, Integer> {
 }
