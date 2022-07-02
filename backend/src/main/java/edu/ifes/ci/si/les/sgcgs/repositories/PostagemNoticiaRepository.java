9bbf8644f0ae62d735f3d7878a14434466097caa
Júlia Souza <juliadsouzaborges@gmail.com>
2022-07-02 15:30:37 -0300

    RN: insert

--- backend/src/main/java/edu/ifes/ci/si/les/sgcgs/repositories/PostagemNoticiaRepository.java
+++ backend/src/main/java/edu/ifes/ci/si/les/sgcgs/repositories/PostagemNoticiaRepository.java
@@ -1,12 +1,13 @@
 package edu.ifes.ci.si.les.sgcgs.repositories;
 
-// import java.util.Collection;
+import java.util.Collection;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+// import edu.ifes.ci.si.les.sgcgs.model.Noticia;
 import edu.ifes.ci.si.les.sgcgs.model.PostagemNoticia;
 // import edu.ifes.ci.si.les.sgcgs.model.Noticia;
  
@@ -19,11 +20,17 @@ public interface PostagemNoticiaRepository extends JpaRepository<PostagemNoticia
         // SELECT USUARIO_ID = 5 FROM NOTICIA WHERE ID = 2 LIMIT 1
         @Transactional(readOnly = true)
         @Query(value = "SELECT USUARIO_ID = ?1 FROM NOTICIA WHERE ID = ?2", nativeQuery = true)
-        public boolean findUsuario(Integer usuarioTentandoAlterar, Integer noticiaID);
+        public Collection<?> findUsuario(Integer usuaruiId, Integer noticiaId);
+        // public boolean findUsuario(Integer usuarioTentandoAlterar, Integer noticiaID);
 
         // REGRA 2:
         // SELECT COUNT(*) < 3 AS VALIDADESTAQUE FROM POSTAGEM_NOTICIA  WHERE DESTAQUE = TRUE LIMIT 1
         @Transactional(readOnly = true)
         @Query(value = "SELECT COUNT(*) < 3 AS VALIDADESTAQUE FROM POSTAGEM_NOTICIA WHERE DESTAQUE = TRUE", nativeQuery = true)
+        // public Collection<?> NoticiaDestaque();
         public boolean findNoticiaDestaque();
+
+        // public Collection<?> findNoticiaDestaque();
+
+        // public Collection<?> findNoticiaDestaque(Boolean boolean1);
 }
--- backend/src/main/java/edu/ifes/ci/si/les/sgcgs/services/PostagemNoticiaService.java
+++ backend/src/main/java/edu/ifes/ci/si/les/sgcgs/services/PostagemNoticiaService.java
@@ -1,41 +1,35 @@
 package edu.ifes.ci.si.les.sgcgs.services;
 
-// import java.util.Collection;
+import java.util.Collection;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-// import edu.ifes.ci.si.les.sgcgs.model.Noticia;
-// import edu.ifes.ci.si.les.sgcgs.model.Usuario;
 import edu.ifes.ci.si.les.sgcgs.model.PostagemNoticia;
-// import edu.ifes.ci.si.les.sgcgs.repositories.NoticiaRepository;
 import edu.ifes.ci.si.les.sgcgs.repositories.PostagemNoticiaRepository;
-// import edu.ifes.ci.si.les.sgcgs.repositories.UsuarioRepository;
+
+
 import edu.ifes.ci.si.les.sgcgs.services.exceptions.ObjectNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
-
-import edu.ifes.ci.si.les.sgcgs.services.exceptions.BusinessRuleException;
 import edu.ifes.ci.si.les.sgcgs.services.exceptions.DataIntegrityException;
 // import edu.ifes.ci.si.les.sgcgs.services.exceptions.ObjectNotFoundException;
+import edu.ifes.ci.si.les.sgcgs.services.exceptions.BusinessRuleException;
 import java.util.NoSuchElementException;
+
+
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /** @author Júlia de Souza Borges */
 
 @Service
 public class PostagemNoticiaService {
     @Autowired
-    private PostagemNoticiaRepository repositoryPN;
-
-    // @Autowired
-    // private NoticiaRepository repositoryNot;
-
-    // @Autowired
-    // private UsuarioRepository repositoryUsr;
+    private PostagemNoticiaRepository repository;
 
     public PostagemNoticia findById(Integer id) {
         // return repository.findById(id).get();
         try{
-            PostagemNoticia obj = repositoryPN.findById(id).get();
+            PostagemNoticia obj = repository.findById(id).get();
             return obj;
         }catch (NoSuchElementException e) {
             throw new ObjectNotFoundException("Notícia não encontrada! ID: " + id + ", Tipo: " + PostagemNoticia.class.getName());
@@ -43,31 +37,34 @@ public class PostagemNoticiaService {
     }
 
     public List<PostagemNoticia> findAll() {
-        return repositoryPN.findAll();
+        return repository.findAll();
     }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW) 
     public PostagemNoticia insert(PostagemNoticia obj) {
         // return repository.save(obj);
         // obj.setId(null);
         try{
-            if (verificaQuantidadeDestaque(obj)){
-                return repositoryPN.save(obj);
-                // obj.setId(null);
+            if(NoticiaDestaque(obj) != true){
+                obj.setId(null);
+                // obj.setNoticia(obj.getNoticia());
+                obj.setDestaque(false);
+                repository.save(obj);
+                throw new BusinessRuleException("Limite máximo de notícias em destaque atingido!");
             }
+            return repository.save(obj);
         }catch (DataIntegrityViolationException e) {
-            throw new DataIntegrityException("222 Campo obrigatório de notícia não foi preenchido!");
+            throw new DataIntegrityException("Campo obrigatório de notícia não foi preenchido!");
         }
-        return null;
-        // return repository.save(obj);
     }
 
     public PostagemNoticia update(PostagemNoticia obj) {
         findById(obj.getId());
         // return repository.save(obj);
         try{
-            return repositoryPN.save(obj);
+            return repository.save(obj);
         } catch (DataIntegrityViolationException e) {
-            throw new DataIntegrityException("Campo obrigatório de notícia não foi preenchido!");
+            throw new DataIntegrityException("2-Campo obrigatório de notícia não foi preenchido!");
         }
     }
 
@@ -75,30 +72,19 @@ public class PostagemNoticiaService {
         // findById(id);
         // repository.deleteById(id);
         try {
-            repositoryPN.deleteById(id);
+            repository.deleteById(id);
         } catch (DataIntegrityViolationException e) {
             throw new DataIntegrityException("Não é possível excluir uma notícia de um outro autor!");
         }
     }
 
-    // Regras de Negócio referente ao processo de negócio Postagem de Notícias
-    // Regra 1: só autor que criou a notícia ou administrador  podem postar ou excluir a postagem
     // Regra 2: validar a quantidade máxima de 3 notícias destaque
-
-    
-    public boolean verificaUsuarioNoticia(PostagemNoticia obj){
-        // Regra 1: só autor que criou a notícia ou administrador  podem postar ou excluir a postagem
-        if (!repositoryPN.findUsuario(obj.getUsuario().getId(), obj.getNoticia().getId())){
-            throw new BusinessRuleException("Não é possível excluir uma notícia de um outro autor!");
+    public boolean NoticiaDestaque(PostagemNoticia obj) {
+        if(repository.findNoticiaDestaque() != true){
+            //throw new BusinessRuleException("Limite máximo de notícias em destaque atingido!");
+            return false;
+        }else{
+           return true;
         }
-        return true;
-    }
-
-    public boolean verificaQuantidadeDestaque(PostagemNoticia obj){
-        // Regra 2: validar a quantidade máxima de 3 notícias destaque
-        if (!repositoryPN.findNoticiaDestaque()){
-            throw new BusinessRuleException("Não é possível adicionar mais notícias em destaque. Limite atingido!");
-        }
-        return true;
     }
 }
