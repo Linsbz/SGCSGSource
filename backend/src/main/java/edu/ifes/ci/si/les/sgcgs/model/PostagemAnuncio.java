79434596674b0fe6c609495fb58b3e15a983acd3
Linsbz <lincolnbazoni1@gmail.com>
2022-07-03 19:24:54 -0300

    Arquivos para o dia 04/07

--- backend/src/main/java/edu/ifes/ci/si/les/sgcgs/model/PostagemAnuncio.java
+++ backend/src/main/java/edu/ifes/ci/si/les/sgcgs/model/PostagemAnuncio.java
@@ -1,43 +1,52 @@
-package edu.ifes.ci.si.les.sgcgs.model;
+// package edu.ifes.ci.si.les.sgcgs.model;
 
-import java.util.Date;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
-import javax.persistence.*;
-import javax.validation.constraints.*;
-import lombok.*;
+// import java.util.Date;
+// import java.io.Serializable;
+// import java.util.ArrayList;
+// import java.util.Collection;
+// import javax.persistence.*;
+// import javax.validation.constraints.*;
+// import lombok.*;
 
-@Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(of = {"id"})
+// @Entity
+// @Data
+// @AllArgsConstructor
+// @NoArgsConstructor
+// @EqualsAndHashCode(of = {"id"})
 
-public class PostagemAnuncio {
+// public class PostagemAnuncio {
 
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+// 	@Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+// 	private Integer id;
 
-	@NotNull(message = "Data do início do anúncio deve ser preenchida")
-	@Column(length = 8)
-    @Size(min = 8, max = 8, message = "Data no formato dd/mm/aaaa")
-	private Date dataInicio;
+// 	@NotNull(message = "Data do início do anúncio deve ser preenchida")
+// 	@Column(length = 8)
+//     @Size(min = 8, max = 8, message = "Data no formato dd/mm/aaaa")
+// 	private Date dataInicio;
 
-	@NotNull(message = "Data do fim do anúncio deve ser preenchida")
-	@Column(length = 8)
-    @Size(min = 8, max = 8, message = "Data no formato dd/mm/aaaa")
-	private Date dataFim;
+// 	@NotNull(message = "Data do fim do anúncio deve ser preenchida")
+// 	@Column(length = 8)
+//     @Size(min = 8, max = 8, message = "Data no formato dd/mm/aaaa")
+// 	private Date dataFim;
 
-	@NotNull(message = "O Anúncio deve ser preenchido")
-    @ManyToOne
-	@JoinColumn(name = "anuncio_id")
-	private Anuncio anuncio;
+// 	@NotNull(message = "O Anúncio deve ser preenchido")
+//     @ManyToOne
+// 	@JoinColumn(name = "anuncio_id")
+// 	private Anuncio anuncio;
 
-	@NotNull(message = "Área de anúncio deve ser preenchida")
-    @ManyToOne
-    @JoinColumn(name = "areasAnuncio_id")
-	private AreaAnuncio areasAnuncio;
+// 	@NotNull(message = "Área de anúncio deve ser preenchida")
+//     @ManyToOne
+//     @JoinColumn(name = "areaAnuncio_id")
+// 	private AreaAnuncio areaAnuncio;
 
-}
+// //     @Builder
+// //     public PostagemAnuncio(Integer id, Date dataI, Date dataF, Anuncio anuncio, AreaAnuncio areaAnuncio) {
+// //         this.id = id;
+// //         this.dataI = data;
+// //         this.dataF = data;
+// //         this.Anuncio = anuncio;
+// //         this.AreaAnuncio = areaAnuncio;
+// //     }
+
+// }
--- backend/src/main/java/edu/ifes/ci/si/les/sgcgs/services/PostagemAnuncioService.java
+++ backend/src/main/java/edu/ifes/ci/si/les/sgcgs/services/PostagemAnuncioService.java
@@ -1,58 +1,106 @@
-package edu.ifes.ci.si.les.sgcgs.services;
+// package edu.ifes.ci.si.les.scv.services;
 
-import java.util.List;
-import java.util.NoSuchElementException;
+// import java.util.Collection;
+// import java.util.NoSuchElementException;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import edu.ifes.ci.si.les.sgcgs.model.PostagemAnuncio;
-import edu.ifes.ci.si.les.sgcgs.repositories.PostagemAnuncioRepository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.dao.DataIntegrityViolationException;
+// import org.springframework.stereotype.Service;
+// import org.springframework.transaction.annotation.Propagation;
+// import org.springframework.transaction.annotation.Transactional;
 
-import org.springframework.dao.DataIntegrityViolationException;
-import edu.ifes.ci.si.les.sgcgs.services.exceptions.DataIntegrityException;
-import edu.ifes.ci.si.les.sgcgs.services.exceptions.ObjectNotFoundException;
+// import edu.ifes.ci.si.les.scv.model.AreaAnuncio;
+// import edu.ifes.ci.si.les.scv.model.PostagemAnuncio;
+// import edu.ifes.ci.si.les.scv.model.Anuncio;
+// import edu.ifes.ci.si.les.scv.repositories.AreaAnuncioRepository;
+// import edu.ifes.ci.si.les.scv.repositories.PostagemAnuncioRepository;
+// import edu.ifes.ci.si.les.scv.repositories.AnuncioRepository;
+// import edu.ifes.ci.si.les.scv.services.exceptions.BusinessRuleException;
+// import edu.ifes.ci.si.les.scv.services.exceptions.DataIntegrityException;
+// import edu.ifes.ci.si.les.scv.services.exceptions.ObjectNotFoundException;
 
-@Service
-public class PostagemAnuncioService {
-    @Autowired
-    private PostagemAnuncioRepository repository;
+// @Service
+// public class PostagemAnuncioService {
 
-    public PostagemAnuncio findById(Integer id) {
-        try{
-            PostagemAnuncio obj = repository.findById(id).get();
-            return obj;
-        }catch (NoSuchElementException e){
-            throw new ObjectNotFoundException("Postagem de Anuncio não encontrada! ID: " + id + ", Tipo: " + PostagemAnuncio.class.getName());
-        }
-    }
+// 	@Autowired
+// 	private PostagemAnuncioRepository postagemAnuncioRepository;
+// 	@Autowired
+// 	private AnuncioRepository AnuncioRepository;
+// 	@Autowired
+// 	private AreaAnuncioRepository AreaAnuncioRepository;
 
-    public List<PostagemAnuncio> findAll() {
-        return repository.findAll();
-    }
+// 	public PostagemAnuncio findById(Integer id) {
+// 		try {
+//         	PostagemAnuncio obj = postagemAnuncioRepository.findById(id).get();
+//         	return obj;
+//         } catch (NoSuchElementException e) {
+//         	throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + PostagemAnuncio.class.getName());
+//         }
+// 	}
 
-    public PostagemAnuncio insert(PostagemAnuncio obj) {
-        obj.setId(null);
-        try{
-            return repository.save(obj);
-        }catch (DataIntegrityViolationException e) {
-            throw new DataIntegrityException("Campo obrigatorio não foi preenchido!");
-        }
-    }
+// 	public Collection<PostagemAnuncio> findAll() {
+// 		return postagemAnuncioRepository.findAll();
+// 	}
 
-    public PostagemAnuncio update(PostagemAnuncio obj) {
-        findById(obj.getId());
-        try{
-            return repository.save(obj);
-        } catch (DataIntegrityViolationException e) {
-            throw new DataIntegrityException("Campo obrigatorio não foi preenchido!");
-        }
-    }
+// 	@Transactional(propagation = Propagation.REQUIRES_NEW) // Esta notação tem objetivo de controlar a propagação da transação (garantindo que sejam realizadas todas as modificações no BD, ou nada)
+// 	public PostagemAnuncio insert(PostagemAnuncio obj) {
+// 		try {
+// 			if (verificarRegrasDeNegocio(obj)) {
+// 				obj.setId(null);
+// 				obj.setAnuncio(obj.Anuncio);
+// 				obj.setAreaAnuncio(obj.AreaAnuncio);
+//                 obj.setdataI(obj.dataI);
+//                 obj.setdataF(obj.dataF);
+// 				return postagemAnuncioRepository.save(obj);
+// 			}
+// 		} catch (DataIntegrityViolationException e) {
+// 			throw new DataIntegrityException("Campo(s) obrigatório(s) do Postagem Anuncio não foi(foram) preenchido(s): AreaAnuncio ou Anuncio");
+// 		}
+// 		return null;
+// 	}
 
-    public void delete(Integer id) {
-        try {
-            repository.deleteById(id);
-        } catch (DataIntegrityViolationException e) {
-            throw new DataIntegrityException("Não é possível excluir uma postagem ativo!");
-        }
-    }
-}
+// 	@Transactional(propagation = Propagation.REQUIRES_NEW) // Esta notação tem objetivo de controlar a propagação da transação (garantindo que sejam realizadas todas as modificações no BD, ou nada)
+// 	public PostagemAnuncio update(PostagemAnuncio obj) {
+// 		try {
+// 			findById(obj.getId());
+// 			return postagemAnuncioRepository.save(obj);
+// 		} catch (DataIntegrityViolationException e) {
+// 			throw new DataIntegrityException("Campo(s) obrigatório(s) do Postagem Anuncio não foi(foram) preenchido(s): AreaAnuncio ou Anuncio");
+// 		}
+// 	}
+
+// 	@Transactional(propagation = Propagation.REQUIRES_NEW) //Todas as operações de persistência no BD serão realizadas em uma única transação (se tudo der certo commit, senão rollback em tudo).
+// 	public void delete(Integer id) {
+// 		findById(id);
+// 		try {
+// 			postagemAnuncioRepository.deleteById(id);
+// 			postagemAnuncioRepository.flush();  // Forçando a sincronização da(s) alteração(ões) e remoção neste momento (assim, diante de qualquer problema, o catch conseguirá capturar a exceção nesta camada de serviço) 
+// 		} catch (DataIntegrityViolationException e) {
+// 			throw new DataIntegrityException("Não é possível excluir este Postagem Anuncio!");
+// 		} 
+// 	}
+
+// 	public boolean verificarRegrasDeNegocio(PostagemAnuncio obj) {
+
+// 		// Regra de Negócio 1: Um anúncio por área de anúncio
+// 		boolean R1 = false;
+// 			// Verificando se existem reservas em aberto para a fita
+// 		Collection <PostagemAnuncio> Col = postagemAnuncioRepository.findByAreaEstado(obj.getId().getAnuncio().getAreaAnuncio());
+// 		if (col.size > 0) {
+// 			R1 = true;
+// 		}
+		
+// 		if (R1) {
+// 			throw new BusinessRuleException("Anuncio em area ja publicada!");
+// 		}
+
+// 		// Regra de Negócio 2: O anuncio só pode estar em uma área de anúncio
+// 		Integer aux = 0;
+// 		// Verificando se existem reservas em aberto para a fita
+// 		Collection <PostagemAnuncio> Col = postagemAnuncioRepository.findByAnuncio(obj.getId().getAnuncio());
+// 		if (col.size>0) {
+// 			throw new BusinessRuleException("Anuncio ja postado em outra area de anuncio!");
+// 		}
+// 	}
+
+//  }
