4e27c15f3e5fc9cc387a2e65ccba0d65e08299b2
Felipe Gaspar <felipesouzalimagaspar@gmail.com>
2022-07-04 15:34:05 -0300

    fix: remove old files

--- .removed/manutencao-de-cadastros/FormularioRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.ifes.ci.si.les.sgcgs.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import edu.ifes.ci.si.les.sgcgs.model.Formulario;
-
-@Repository
-public interface FormularioRepository extends JpaRepository<Formulario, Integer> {
-}
