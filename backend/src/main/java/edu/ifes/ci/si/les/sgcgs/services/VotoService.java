01774bfcc2dd06c4a0517ed35799892565563229
HildaBiazatti <hildabborges@gmail.com>
2022-07-04 14:32:06 -0300

    correção dos itens de Voto conflitantes

--- backend/src/main/java/edu/ifes/ci/si/les/sgcgs/services/VotoService.java
+++ backend/src/main/java/edu/ifes/ci/si/les/sgcgs/services/VotoService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package edu.ifes.ci.si.les.sgcgs.services;
 
 import java.util.List;
@@ -86,40 +85,3 @@ public class VotoService {
         }
 	}
 }
-=======
-package edu.ifes.ci.si.les.sgcgs.services;
-
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import edu.ifes.ci.si.les.sgcgs.model.Voto;
-import edu.ifes.ci.si.les.sgcgs.repositories.VotoRepository;
-
-@Service
-public class VotoService {
-    @Autowired
-    private VotoRepository repository;
-
-    public Voto findById(Integer id) {
-        return repository.findById(id).get();
-    }
-
-    public List<Voto> findAll() {
-        return repository.findAll();
-    }
-
-    public Voto insert(Voto obj) {
-        return repository.save(obj);
-    }
-
-    public Voto update(Voto obj) {
-        findById(obj.getId());
-        return repository.save(obj);
-    }
-
-    public void delete(Integer id) {
-        findById(id);
-        repository.deleteById(id);
-    }
-}
->>>>>>> refs/remotes/origin/main
